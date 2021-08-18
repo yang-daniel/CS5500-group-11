2cd8b335c5cb47d4f2fb0cf3a3dc9e3f90cdee5d
Brittnee Braun <brittneeb@gmail.com>
2021-08-17 23:22:02 -0400

    added graphs to webpages

--- fitness-application/src/main/controller/DayControllerWeb.java
+++ fitness-application/src/main/controller/DayControllerWeb.java
@@ -11,15 +11,22 @@ import main.model.Summary;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @Controller
@@ -43,57 +50,37 @@ public class DayControllerWeb {
     //     return this.dayService.getDayByDate(date);
     // 
 
-    @GetMapping("/web/day/{date}")
-    public String getDay(@PathVariable("date") String date, Model model) {
-        model.addAttribute("day", this.dayService.getDayByDate(date));
-        model.addAttribute("formatted_date", this.formatDateFancy(date));
-        // System.out.println("Day Controller: " + date);
-
-        // Take the request and fill this.days;
-        // this.days = dayService.getDayByDate(date);
-        // System.out.println("Day has " + this.days.getCaloriesIdle() + " calories");
-
-        // List<Day> day = dayService.getAllDays();
-
-        // if(day.isEmpty()){
-
-        //     //model.addAttribute("name", "No Match");
-        //     model.addAttribute("name", dayService.getAllDays());
-        // }
-        // else {
-        //     //model.addAttribute("name", date);
-
-        //     model.addAttribute("name", dayService.getAllDays());
-        //     //model.addAttribute("name", day.get().getDate());
-        // }
-
+    @GetMapping("/web/day")
+    public String getDay(Model model) {
         return "day";
     }
+
+
+
 
     @GetMapping("web/day/{date}/rewards")
     public String getRewards(@PathVariable("date") String date, Model model) {
         Day d = this.dayService.getDayByDate(date);
         model.addAttribute("day", d);
-        model.addAttribute("formatted_date", this.formatDateFancy(date));
 
         try {
             model.addAttribute("listsum", d.getSummary());
         } catch (Exception e) {
             model.addAttribute("listsum", "No data for input");
         }
-        
-        int totalSteps = 0;
-        List<Summary> summaries = d.getSummary();
-        for (Summary s : summaries) {
-            if (s.getActivity().equals("walking")) {
-                totalSteps += s.getSteps();
-            }
-        }
-        model.addAttribute("total_steps", totalSteps);
 
         List<Day> listsum = new ArrayList<>();
+        List<List<Object>> dpointsInt = new ArrayList<List<Object>>();
         if (date.length() == 8) {
+            int dayCals = 0;
+            for (Summary s : d.getSummary()) {
+                dayCals += s.getCalories();
+            }
+            dayCals += d.getCaloriesIdle();
+
             String mthYear = date.substring(0, 6);
+            String month = mthYear.substring(4,6);
+            String year = mthYear.substring(0, 4);
             for (int i = 1; i < 32; i++ ) {
                 String mthDate = mthYear;
                 if (i < 10) {
@@ -103,11 +90,23 @@ public class DayControllerWeb {
                 }
                 Day mthD = this.dayService.getDayByDate(mthDate);
                 if (mthD != null) {
+                    int totalCals = 0;
+                    for (Summary s : mthD.getSummary()) {
+                        totalCals += s.getCalories();
+                    }
+                    totalCals += mthD.getCaloriesIdle();
                     listsum.add(mthD);
+                    List<Object> map = new ArrayList<Object>();
+                    String graphDate = month + "/" + i + "/" + year;
+                    map.add(graphDate);
+                    map.add(totalCals * 2);
+                    map.add(dayCals * 2);
+                    dpointsInt.add(map);
                 }
             }
         }
         model.addAttribute("monthSum", listsum);
+        model.addAttribute("monthDataPoints", dpointsInt);
         return "rewards";
     }
 
@@ -115,26 +114,25 @@ public class DayControllerWeb {
     public String getSteps(@PathVariable("date") String date, Model model) {
         Day d = this.dayService.getDayByDate(date);
         model.addAttribute("day", d);
-        model.addAttribute("formatted_date", this.formatDateFancy(date));
 
         try {
             model.addAttribute("listsum", d.getSummary());
         } catch (Exception e) {
             model.addAttribute("listsum", "No data for input");
         }
-        
-        int totalSteps = 0;
-        List<Summary> summaries = d.getSummary();
-        for (Summary s : summaries) {
-            if (s.getActivity().equals("walking")) {
-                totalSteps += s.getSteps();
-            }
-        }
-        model.addAttribute("total_steps", totalSteps);
 
         List<Day> listsum = new ArrayList<>();
+        List<List<Object>> dpointsInt = new ArrayList<List<Object>>();
         if (date.length() == 8) {
+            int dayCals = 0;
+            for (Summary s : d.getSummary()) {
+                dayCals += s.getCalories();
+            }
+            dayCals += d.getCaloriesIdle();
+
             String mthYear = date.substring(0, 6);
+            String month = mthYear.substring(4,6);
+            String year = mthYear.substring(0, 4);
             for (int i = 1; i < 32; i++ ) {
                 String mthDate = mthYear;
                 if (i < 10) {
@@ -144,19 +142,30 @@ public class DayControllerWeb {
                 }
                 Day mthD = this.dayService.getDayByDate(mthDate);
                 if (mthD != null) {
+                    int totalCals = 0;
+                    for (Summary s : mthD.getSummary()) {
+                        totalCals += s.getCalories();
+                    }
+                    totalCals += mthD.getCaloriesIdle();
                     listsum.add(mthD);
+                    List<Object> map = new ArrayList<Object>();
+                    String graphDate = month + "/" + i + "/" + year;
+                    map.add(graphDate);
+                    map.add(totalCals);
+                    map.add(dayCals);
+                    dpointsInt.add(map);
                 }
             }
         }
         model.addAttribute("monthSum", listsum);
+        model.addAttribute("monthDataPoints", dpointsInt);
         return "steps";
     }
 
     @GetMapping("web/day/{date}/calories")
-    public String getCalories(@PathVariable("date") String date, Model model) {
+    public String getCalories(@PathVariable("date") String date, Model model) throws ParseException {
         Day d = this.dayService.getDayByDate(date);
         model.addAttribute("day", d);
-        model.addAttribute("formatted_date", this.formatDateFancy(date));
 
         try {
             model.addAttribute("listsum", d.getSummary());
@@ -165,8 +174,17 @@ public class DayControllerWeb {
         }
 
         List<Day> listsum = new ArrayList<>();
+        List<List<Object>> dpointsInt = new ArrayList<List<Object>>();
         if (date.length() == 8) {
+            int dayCals = 0;
+            for (Summary s : d.getSummary()) {
+                dayCals += s.getCalories();
+            }
+            dayCals += d.getCaloriesIdle();
+
             String mthYear = date.substring(0, 6);
+            String month = mthYear.substring(4,6);
+            String year = mthYear.substring(0, 4);
             for (int i = 1; i < 32; i++ ) {
                 String mthDate = mthYear;
                 if (i < 10) {
@@ -176,13 +194,26 @@ public class DayControllerWeb {
                 }
                 Day mthD = this.dayService.getDayByDate(mthDate);
                 if (mthD != null) {
+                    int totalCals = 0;
+                    for (Summary s : mthD.getSummary()) {
+                        totalCals += s.getCalories();
+                    }
+                    totalCals += mthD.getCaloriesIdle();
                     listsum.add(mthD);
+                    List<Object> map = new ArrayList<Object>();
+                    String graphDate = month + "/" + i + "/" + year;
+                    map.add(graphDate);
+                    map.add(totalCals);
+                    map.add(dayCals);
+                    dpointsInt.add(map);
                 }
             }
         }
+        model.addAttribute("monthDataPoints", dpointsInt);
         model.addAttribute("monthSum", listsum);
         return "calories";
     }
+
 
     private String formatDateFancy(String date) {
         if (date.length() != 8) {
